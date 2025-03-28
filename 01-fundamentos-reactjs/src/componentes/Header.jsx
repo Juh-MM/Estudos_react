294939d55475949c6c818d5e699b92d25425491b
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-03-28 03:12:25 +0000

    new file:   01-fundamentos-reactjs/src/assets/Ignite-simbol.svg 	modified:   01-fundamentos-reactjs/src/componentes/Header.jsx 	modified:   01-fundamentos-reactjs/src/componentes/Header.module.css

--- 01-fundamentos-reactjs/src/componentes/Header.jsx
+++ 01-fundamentos-reactjs/src/componentes/Header.jsx
@@ -1,8 +1,11 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/Ignite-simbol.svg'
+
 export function Header() {
     return (
         <header className={styles.header}>
+            <img src={igniteLogo} alt="logo do Ignite" />
             <strong>Ignite Feed</strong>
         </header>
     );
