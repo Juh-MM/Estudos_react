8b2ea086d1c09034abb367591cfc0db06d2f7dcb
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2025-03-28 17:03:15 +0000

    modified:   01-fundamentos-reactjs/src/App.jsx 	new file:   01-fundamentos-reactjs/src/App.module.css 	new file:   01-fundamentos-reactjs/src/componentes/Sidebar.jsx 	new file:   01-fundamentos-reactjs/src/componentes/Sidebar.module.css

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,19 +1,31 @@
 import { Header } from './componentes/Header';
 import { Post } from './Post';
+
 import './global.css'
+import styles from './App.module.css'
+import { Siderbar } from './componentes/Sidebar';
+
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-      author="julia martins"
-      content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit eos earum laborum blanditiis"
-      />
-      <Post
-      author="gabriel carvalho"
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, officiis libero laudantium sequi in adipisci nisi numquam iste. Corporis debitis nobis culpa est harum, facilis ipsa fugiat unde iure tempora!"
-      ></Post>
+      <div className={styles.wrapper}>
+
+        <Siderbar />
+
+        <main>
+          <Post 
+          author="julia martins"
+          content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit eos earum laborum blanditiis"
+          />
+          <Post
+          author="gabriel carvalho"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, officiis libero laudantium sequi in adipisci nisi numquam iste. Corporis debitis nobis culpa est harum, facilis ipsa fugiat unde iure tempora!"
+          ></Post>
+        </main>
+        
+      </div>
     </div>
   );
 }
---
+++ 01-fundamentos-reactjs/src/componentes/Sidebar.jsx
@@ -0,0 +1,17 @@
+import styles from './Sidebar.module.css'
+
+export function Siderbar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1743021192936-56644cc30ac7?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <div className={styles.profile}>
+                <strong>Júlia Martins</strong>
+                <span>Web developer</span>
+            </div>
+            <footer>
+                <a href="#">Editar seu perfil</a>
+            </footer>
+        </aside>
+
+    );
+}
